d57095dee4b692f18e8d68387cc909802c68bece
Adrien Mergalet <22292245+AdrienMergalet@users.noreply.github.com>
2022-06-04 23:23:07 +0200

    Switch to axios

--- front/src/hooks/api.ts
+++ front/src/hooks/api.ts
@@ -1,79 +1,70 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import axios, { AxiosRequestHeaders } from 'axios';
+
+const reqInstance = axios.create();
+
+const headers: AxiosRequestHeaders = {
+  'Content-Type': 'application/json',
+};
 
 export function useApi() {
   const { getAccessTokenSilently } = useAuth0();
 
   const apiUrl = process.env.API_URL;
-  const headers: HeadersInit = {
-    'Content-Type': 'application/json',
-  };
 
   async function get<ResType>(endpoint: string) {
     const token = await getAccessTokenSilently();
-    const res = await fetch(`${apiUrl}${endpoint}`, {
-      method: 'GET',
+    const res = await reqInstance.get<ResType>(`${apiUrl}${endpoint}`, {
       headers: {
         ...headers,
         Authorization: `Bearer ${token}`,
       },
     });
-    if (res.ok) return (await res.json()) as ResType;
-    return await res.json();
+    return res.data;
   }
 
   async function post<ResType, ReqType>(endpoint: string, body?: ReqType) {
     const token = await getAccessTokenSilently();
-    const res = await fetch(`${apiUrl}${endpoint}`, {
-      method: 'POST',
+    const res = await reqInstance.post<ResType>(`${apiUrl}${endpoint}`, body, {
       headers: {
         ...headers,
         Authorization: `Bearer ${token}`,
       },
-      body: body ? JSON.stringify(body) : undefined,
     });
-    if (res.ok) return (await res.json()) as ResType;
-    return await res.json();
+    return res.data;
   }
 
   async function put<ResType, ReqType>(endpoint: string, body?: ReqType) {
     const token = await getAccessTokenSilently();
-    const res = await fetch(`${apiUrl}${endpoint}`, {
-      method: 'PUT',
+    const res = await reqInstance.put<ResType>(`${apiUrl}${endpoint}`, body, {
       headers: {
         ...headers,
         Authorization: `Bearer ${token}`,
       },
-      body: body ? JSON.stringify(body) : undefined,
     });
-    if (res.ok) return (await res.json()) as ResType;
-    return await res.json();
+    return res.data;
   }
 
   async function patch<ResType, ReqType>(endpoint: string, body?: ReqType) {
     const token = await getAccessTokenSilently();
-    const res = await fetch(`${apiUrl}${endpoint}`, {
-      method: 'PATCH',
+    const res = await reqInstance.patch<ResType>(`${apiUrl}${endpoint}`, body, {
       headers: {
         ...headers,
         Authorization: `Bearer ${token}`,
       },
-      body: body ? JSON.stringify(body) : undefined,
     });
-    if (res.ok) return (await res.json()) as ResType;
-    return await res.json();
+    return res.data;
   }
 
   async function del<ResType>(endpoint: string) {
     const token = await getAccessTokenSilently();
-    const res = await fetch(`${apiUrl}${endpoint}`, {
-      method: 'DELETE',
+    const res = await reqInstance.delete<ResType>(`${apiUrl}${endpoint}`, {
       headers: {
         ...headers,
         Authorization: `Bearer ${token}`,
       },
     });
-    if (res.ok) return (await res.json()) as ResType;
-    return await res.json();
+    return res.data;
   }
 
   return { get, post, put, patch, del };
