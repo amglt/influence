d6ab54af48750d39a2c47e966f2b24c92cd29484
Adrien Mergalet <22292245+AdrienMergalet@users.noreply.github.com>
2022-08-09 20:37:02 +0200

    feat: bot-ci

--- bot/setup.py
+++ bot/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='influbot',
     version='0.1',
-    packages=['tests', 'influbot', 'influbot.api', 'influbot.events', 'influbot.shared', 'influbot.handlers'],
+    packages=['.', 'api', 'events', 'shared', 'handlers'],
     url='',
     license='',
     author='Adrien Mergalet',
