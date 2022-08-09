bc32f567c1c0bf8492ff692ed11a594dff250df4
Adrien Mergalet <22292245+AdrienMergalet@users.noreply.github.com>
2022-08-09 14:57:59 +0200

    fix: env variables

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,8 +3,6 @@ import './styles/root.less';
 import { Providers } from './providers';
 import { Router } from './Router';
 
-console.log(process.env);
-
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
