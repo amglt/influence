1671f81381b953880183e0d180d88e4ceaca4b85
Adrien Mergalet <22292245+AdrienMergalet@users.noreply.github.com>
2022-08-29 11:18:58 +0200

    update: multiple channels

--- bot/influbot/events/message.py
+++ bot/influbot/events/message.py
@@ -26,7 +26,9 @@ async def handle_message(client: Client, message: Message):
                 await handle_remove_transaction(message)
             if content.startswith('.a') or content.startswith('.ava'):
                 await handle_ava(client, message)
-        if message.channel.id == int(os.getenv("PVP_INFO_CHANNEL_ID")):
+
+        pvp_info_channels = [int(x) for x in os.getenv("PVP_INFO_CHANNEL_ID").split(',')]
+        if message.channel.id in pvp_info_channels:
             if content.startswith('.ms') or content.startswith('.mystats'):
                 await handle_my_stats(message)
             if content.startswith('.pvp'):
@@ -34,7 +36,8 @@ async def handle_message(client: Client, message: Message):
 
     # Influtons
     if EnabledModules.Influtons.value in enabled_modules:
-        if message.channel.id == int(os.getenv("INFLUTONS_CHANNEL_ID")):
+        influtons_channels = [int(x) for x in os.getenv("INFLUTONS_CHANNEL_ID").split(',')]
+        if message.channel.id in influtons_channels:
             if content.startswith('.give'):
                 await handle_influtons_request(message)
             if content.startswith('.money'):
